18e0853f79b30ad28bc1e7fdc16ebe8f0ea87553
Devin <devinj1121@gmail.com>
2023-05-03 19:55:44 -0500

    commit

--- MultiSelectDelete_0.1.1/control.lua
+++ MultiSelectDelete_0.1.1/control.lua
@@ -1,12 +1,28 @@
+function euclidean_distance(x1, y1, x2, y2)
+	return math.sqrt((x2-x1)^2 + (y2-y1)^2)
+end
+
 local function on_player_selected_area(event)
     local player = game.players[event.player_index]
-    for key, entity in pairs(player.surface.find_entities_filtered{area=event.area,}) do
-        player.mine_entity(entity, true)
+
+    for key, entity in pairs(player.surface.find_entities_filtered{area=event.area}) do
+        
+        -- Get euclidean distance from player to entity
+        d = euclidean_distance(entity.position.x, entity.position.y, player.position.x, player.position.y)
+
+        -- If entity is more than 2 * reach away, raise error
+        if d >= 2*player.reach_distance then
+            player.print("Item is too far!") -- Need a way for this to access localization file isntead of harcoded
+        -- Otherwise mine it
+        else
+            player.mine_entity(entity, true)
+        end
     end
 end
 
 local function handle_shortcut(event)
 
+    -- Get the player who triggered the event
     local player = game.players[event.player_index]
     
     -- Bring tool to cursor
@@ -16,6 +32,5 @@ local function handle_shortcut(event)
     script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
 end
 
-    
-
 script.on_event("shortcut", handle_shortcut)
+
--- MultiSelectDelete_0.1.1/data.lua
+++ MultiSelectDelete_0.1.1/data.lua
@@ -2,14 +2,13 @@ local multiselector_tool = {
     type = "selection-tool",
     name = "multiselect-delete",
     subgroup = "tool",
-    order = "za[multiselect-delete]",
     icons = {
       {
         icon = "__MultiSelectDelete__/graphics/icons/selection.png",
         icon_size = 32,
       }
     },
-    flags = {},
+    flags = {"only-in-cursor", "hidden"}, -- Makes sure that not added to inventory
     stack_size = 1,
     stackable = false,
     selection_color = {r = 1, g = 0, b = 0, a = 1},
@@ -20,14 +19,7 @@ local multiselector_tool = {
     alt_selection_cursor_box_type = "not-allowed",
   }
 
-local multiselector_recipe = {
-    type = "recipe", 
-    name = "multiselect-delete",
-    enabled = true,
-    ingredients = {},
-    result = "multiselect-delete",
-}
-
+  -- TODO: I think we need to add this to settings
 local tool_shortcut = {
   type = "custom-input",
   name = "shortcut", -- this is the name of the event raised
@@ -35,4 +27,4 @@ local tool_shortcut = {
   action = "lua" 
 }
 
-data:extend({multiselector_tool, multiselector_recipe, tool_shortcut})
+data:extend({multiselector_tool, tool_shortcut})
